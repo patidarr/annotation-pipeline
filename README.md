6d74413e023e81c6afb1bfec81d5349d44303397
Rajesh Patidar <rajbtpatidar@gmail.com>
2015-07-17 12:27:58 -0400

    Added mutational cancer driver gene based annotations

--- README.md
+++ README.md
@@ -118,6 +118,19 @@ NIH Users can create account and access the database [here]. Enter the code 1881
 #####This update does not contain the name of diagnosis as annotation but it contains the link to the website which can give much more detail information about the mutation
 #####Becase the genomic changes are based on backlocation; the cDNA change in database and the mutation in question may not be same.
 
+####[mutational cancer driver genes across 12] Gene Level annotation
+[mutational cancer driver genes across 12]: http://www.nature.com/srep/2013/131002/srep02650/full/srep02650.html
+
+Follwing columns are directly from the paper. Supplimentary Table 2.
+- **Cancer Gene Census**
+- **MuSIC**:
+- **OncodriveFM**:
+- **OncodriveCLUST**:
+- **ActiveDriver**:
+- **MutSig**:
+- **Putative Driver Category**:
+
+
 - **[Total_ICGC]** No of sample have mutation in ICGC population (~6500 Patients, ~50 Cancer types)
 [Total_ICGC]: https://icgc.org/
 - **[Count_TCGA_ALL]** (~16K Samples)
--- combine.sh
+++ combine.sh
@@ -15,11 +15,13 @@ $TOOL $FILE.hgmd     $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar >$FILE.a
 $TOOL $FILE.match    $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match
 $TOOL $FILE.docm     $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm 
 $TOOL $FILE.mcg      $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg
-$TOOL $FILE.anno.pcg           $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg
-$TOOL $FILE.uvm      $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm
-$TOOL $FILE.germline      $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm.germline
-$CODE/GeneAnnotation.pl $DATADIR/hg19_ACMG.txt $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm.germline >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm.germline.acmg
-cp $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm.germline.acmg $FILE.annotations.txt
+$CODE/GeneAnnotation.pl $DATADIR/hg19_mutationalcancerDriver.txt $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver
+$TOOL $FILE.anno.pcg           $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg
+$TOOL $FILE.uvm      $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm
+$TOOL $FILE.germline      $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm.germline
+$CODE/GeneAnnotation.pl $DATADIR/hg19_ACMG.txt $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm.germline >$FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm.germline.acmg
+
+cp $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm.germline.acmg $FILE.annotations.txt
 
 $CODE/joinAllAnnotations.pl $FILE.annotations.txt $FILE >$FILE.annotations.final.txt
 
@@ -29,6 +31,6 @@ rm -rf SIFT.e* SIFT.o* PPH2.e* PPH2.o* ANNOVAR.e* ANNOVAR.o* swb*.o* swb*.e* pph
 
 rm -rf $FILE.anno $FILE.pph $FILE.sift $FILE.sift_predictions.tsv $FILE.sift.out $FILE.pph2 $FILE.anno.gene $FILE.anno.exac.3 $FILE.anno.clinseq  $FILE.pph2.out $FILE.polyphen2.out $FILE.anno.cadd $FILE.anno.clinvar $FILE.anno.pcg $FILE.hgmd $FILE.match $FILE.mcg $FILE.docm $FILE.germline $FILE.uvm
 
-rm -rf $FILE.anno.gene.exac $FILE.anno.gene.exac.clinseq $FILE.anno.gene.exac.clinseq.cadd $FILE.anno.gene.exac.clinseq.cadd.sift $FILE.anno.gene.exac.clinseq.cadd.sift.pph $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm.germline $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.pcg.uvm.germline.acmg 
+rm -rf $FILE.anno.gene.exac $FILE.anno.gene.exac.clinseq $FILE.anno.gene.exac.clinseq.cadd $FILE.anno.gene.exac.clinseq.cadd.sift $FILE.anno.gene.exac.clinseq.cadd.sift.pph $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm.germline $FILE.anno.gene.exac.clinseq.cadd.sift.pph.clinvar.hgmd.match.docm.mcg.driver.pcg.uvm.germline.acmg 
 
 echo -e "Annotation Pipeline finished on File $FILE\n Please collect result from $DIR \n\nRegards, \nOncogenomics Section\nNCI" |mutt  -s "Annotation Pipeline Status"  `whoami`@mail.nih.gov -c patidarr@mail.nih.gov
